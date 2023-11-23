ad967de666252e057c455121ddec882a3ec3bb90
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-23 14:00:50 +0000

    define initial class structure

--- PoEAAMoney.Tests/UnitTest1.cs
+++ PoEAAMoney.Tests/UnitTest1.cs
@@ -1,10 +1,25 @@
 namespace PoEAAMoney.Tests;
 
-public class UnitTest1
+public enum Currency
+{
+    BRL,
+    USD,
+    EUR
+}
+
+class Money(decimal amount, Currency currency)
+{
+    public readonly decimal Amount = amount;
+    public readonly Currency Currency = currency;
+}
+
+public class MoneyTests
 {
     [Fact]
-    public void Test1()
+    public void TestHasAmountAndCurrency()
     {
-
+        var money = new Money(amount: 10, currency: Currency.BRL);
+        Assert.Equal(10, money.Amount);
+        Assert.Equal(Currency.BRL, money.Currency);
     }
 }
