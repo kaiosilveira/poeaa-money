9a2ed6bce587ca8c28ee9cabc0b58222c2c0796b
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-25 09:58:04 +0000

    move arithmetic tests to its own file

--- PoEAAMoney.Tests/UnitTest1.cs
+++ PoEAAMoney.Tests/UnitTest1.cs
@@ -1,58 +1,5 @@
 namespace PatternsOfEnterpriseApplicationArchitecture.BasePatterns.Tests;
 
-public class MoneyTests_Arithmetics
-{
-    [Fact]
-    public void TestAddsUp()
-    {
-        var tenEuros = Money.Euros(amount: 10);
-        var twentyEuros = Money.Euros(amount: 20);
-        var thirtyEuros = Money.Euros(amount: 30);
-        Assert.Equal(thirtyEuros, tenEuros + twentyEuros);
-    }
-
-    [Fact]
-    public void TestCannotAddDifferentCurrencies()
-    {
-        var tenEuros = Money.Euros(amount: 10);
-        var tenDollars = Money.Dollars(amount: 10);
-        Assert.Throws<InvalidOperationException>(() => tenEuros + tenDollars);
-    }
-
-    [Fact]
-    public void TestSubtracts()
-    {
-        var thirtyEuros = Money.Euros(amount: 30);
-        var tenEuros = Money.Euros(amount: 10);
-        var twentyEuros = Money.Euros(amount: 20);
-        Assert.Equal(twentyEuros, thirtyEuros - tenEuros);
-    }
-
-    [Fact]
-    public void TestCannotSubtractDifferentCurrencies()
-    {
-        var tenEuros = Money.Euros(amount: 10);
-        var tenDollars = Money.Dollars(amount: 10);
-        Assert.Throws<InvalidOperationException>(() => tenEuros - tenDollars);
-    }
-
-    [Fact]
-    public void TestMultiplication()
-    {
-        var tenEuros = Money.Euros(amount: 10);
-        var twentyEuros = Money.Euros(amount: 20);
-        Assert.Equal(twentyEuros, tenEuros * 2);
-    }
-
-    [Fact]
-    public void TestRoundsToHalfEvenWhenMultiplying()
-    {
-        var oneEuro = Money.Euros(amount: 1);
-        Assert.Equal(Money.Euros(amount: 2.76m), oneEuro * 2.756m);
-        Assert.Equal(Money.Euros(amount: 2.75m), oneEuro * 2.754m);
-    }
-}
-
 public class MoneyTests_Equality
 {
     [Fact]
