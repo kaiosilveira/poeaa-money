c15fba028162e427ac4a2ba6a52428e454408ad9
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-25 09:57:35 +0000

    move inequality tests to its own file

--- PoEAAMoney.Tests/UnitTest1.cs
+++ PoEAAMoney.Tests/UnitTest1.cs
@@ -1,26 +1,5 @@
 namespace PatternsOfEnterpriseApplicationArchitecture.BasePatterns.Tests;
 
-public class MoneyTests_Inequality
-{
-    [Fact]
-    public void TestGreaterThanComparisons()
-    {
-        var tenEuros = Money.Euros(amount: 10);
-        var elevenEuros = Money.Euros(amount: 11);
-
-        Assert.True(elevenEuros > tenEuros);
-        Assert.True(tenEuros < elevenEuros);
-    }
-
-    [Fact]
-    public void TestCannotCompareDifferentCurrencies()
-    {
-        var tenEuros = Money.Euros(amount: 10);
-        var tenDollars = Money.Dollars(amount: 10);
-        Assert.Throws<InvalidOperationException>(() => tenEuros > tenDollars);
-    }
-}
-
 public class MoneyTests_Arithmetics
 {
     [Fact]
