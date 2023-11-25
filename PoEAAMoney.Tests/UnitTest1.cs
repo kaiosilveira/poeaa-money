52d1e5f41b94cd68aa8551bfa7e39f41e300ad21
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-25 09:58:37 +0000

    move equality tests to its own file

--- PoEAAMoney.Tests/UnitTest1.cs
+++ PoEAAMoney.Tests/UnitTest1.cs
@@ -1,32 +1,5 @@
 namespace PatternsOfEnterpriseApplicationArchitecture.BasePatterns.Tests;
 
-public class MoneyTests_Equality
-{
-    [Fact]
-    public void TestIsEqualSameAmountSameCurrency()
-    {
-        var tenEuros1 = Money.Euros(amount: 10);
-        var tenEuros2 = Money.Euros(amount: 10);
-        Assert.Equal(tenEuros1, tenEuros2);
-    }
-
-    [Fact]
-    public void TestIsNotEqualDifferentAmountSameCurrency()
-    {
-        var tenEuros = Money.Euros(amount: 10);
-        var twentyEuros = Money.Euros(amount: 20);
-        Assert.NotEqual(tenEuros, twentyEuros);
-    }
-
-    [Fact]
-    public void TestIsNotEqualSameAmountDifferentCurrency()
-    {
-        var tenEuros = Money.Euros(amount: 10);
-        var tenDollars = Money.Dollars(amount: 10);
-        Assert.NotEqual(tenEuros, tenDollars);
-    }
-}
-
 public class MoneyTests_Shortcuts
 {
     [Fact]
---
+++ PoEAAMoney.Tests/src/Money/Equality.cs
@@ -0,0 +1,28 @@
+namespace PatternsOfEnterpriseApplicationArchitecture.BasePatterns.Tests;
+
+public class MoneyTests_Equality
+{
+    [Fact]
+    public void TestIsEqualSameAmountSameCurrency()
+    {
+        var tenEuros1 = Money.Euros(amount: 10);
+        var tenEuros2 = Money.Euros(amount: 10);
+        Assert.Equal(tenEuros1, tenEuros2);
+    }
+
+    [Fact]
+    public void TestIsNotEqualDifferentAmountSameCurrency()
+    {
+        var tenEuros = Money.Euros(amount: 10);
+        var twentyEuros = Money.Euros(amount: 20);
+        Assert.NotEqual(tenEuros, twentyEuros);
+    }
+
+    [Fact]
+    public void TestIsNotEqualSameAmountDifferentCurrency()
+    {
+        var tenEuros = Money.Euros(amount: 10);
+        var tenDollars = Money.Dollars(amount: 10);
+        Assert.NotEqual(tenEuros, tenDollars);
+    }
+}
